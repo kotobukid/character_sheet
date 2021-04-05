6d69d5e2fdb9524e58eff30d7479668a87bf9fe8
kotobukid <kotobuki@daqua.jp>
2021-04-05 23:16:06 +0900

    CDNでReactを動かす準備

--- character_sheet/settings.py
+++ character_sheet/settings.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'character_sheet.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': os.path.join(BASE_DIR, 'templates')
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), ]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
