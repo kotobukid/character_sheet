93cbb66b56136699fef16f5d2940d610fed4c514
kotobukid <kotobuki@daqua.jp>
2021-05-04 23:38:42 +0900

    スタイルはまだ分からん

--- ts/components/base_status.tsx
+++ ts/components/base_status.tsx
@@ -3,22 +3,31 @@ import {BaseStatusData} from "../types"
 import SqInput from "./parts/sq_input";
 
 const BaseStatus: React.FC<BaseStatusData> = (props) => {
+    const ColStyle: React.CSSProperties = {width: "60px"}
     return (
         <div id="base_status">
             <table>
+                <colgroup>
+                    <col style={ColStyle}/>
+                    <col style={ColStyle}/>
+                    <col style={ColStyle}/>
+                    <col style={ColStyle}/>
+                    <col style={ColStyle}/>
+                </colgroup>
+
                 <tbody>
                 <tr>
                     <td rowSpan={2}><br /><SqInput value={props.skill} digit={3} onchange={(value: string) => {props.setSkill(Number(value))}}>技</SqInput></td>
                     <td><SqInput value={props.a} digit={3} onchange={(value: string) => {props.setA(Number(value))}}>A</SqInput></td>
                     <td><SqInput value={props.a_growth} digit={3} onchange={(value: string) => {props.setAGrowth(Number(value))}}>成長</SqInput></td>
                     <td>器用度<br />{props.skill + props.a + props.a_growth}</td>
-                    <td>{Math.floor((props.skill + props.a + props.a_growth) / 6)}</td>
+                    <td className="bold center large">{Math.floor((props.skill + props.a + props.a_growth) / 6)}</td>
                 </tr>
                 <tr>
                     <td><SqInput value={props.b} digit={3} onchange={(value: string) => {props.setB(Number(value))}}>B</SqInput></td>
                     <td><SqInput value={props.b_growth} digit={3} onchange={(value: string) => {props.setBGrowth(Number(value))}} /></td>
                     <td>敏捷度<br />{props.skill + props.b + props.b_growth}</td>
-                    <td>{Math.floor((props.skill + props.b + props.b_growth) / 6)}</td>
+                    <td className="bold center large">{Math.floor((props.skill + props.b + props.b_growth) / 6)}</td>
                 </tr>
 
                 <tr>
@@ -26,13 +35,13 @@ const BaseStatus: React.FC<BaseStatusData> = (props) => {
                     <td><SqInput value={props.c} digit={3} onchange={(value: string) => {props.setC(Number(value))}}>C</SqInput></td>
                     <td><SqInput value={props.c_growth} digit={3} onchange={(value: string) => {props.setCGrowth(Number(value))}} /></td>
                     <td>筋力<br />{props.body + props.c + props.c_growth}</td>
-                    <td>{Math.floor((props.body + props.c + props.c_growth) / 6)}</td>
+                    <td className="bold center large">{Math.floor((props.body + props.c + props.c_growth) / 6)}</td>
                 </tr>
                 <tr>
                     <td><SqInput value={props.d} digit={3} onchange={(value: string) => {props.setD(Number(value))}}>D</SqInput></td>
                     <td><SqInput value={props.d_growth} digit={3} onchange={(value: string) => {props.setDGrowth(Number(value))}} /></td>
                     <td>生命力<br />{props.body + props.d + props.d_growth}</td>
-                    <td>{Math.floor((props.body + props.d + props.d_growth) / 6)}</td>
+                    <td className="bold center large">{Math.floor((props.body + props.d + props.d_growth) / 6)}</td>
                 </tr>
 
                 <tr>
@@ -40,13 +49,13 @@ const BaseStatus: React.FC<BaseStatusData> = (props) => {
                     <td><SqInput value={props.e} digit={3} onchange={(value: string) => {props.setE(Number(value))}}>E</SqInput></td>
                     <td><SqInput value={props.e_growth} digit={3} onchange={(value: string) => {props.setEGrowth(Number(value))}} /></td>
                     <td>知力<br />{props.mental + props.e + props.e_growth}</td>
-                    <td>{Math.floor((props.mental + props.e + props.e_growth) / 6)}</td>
+                    <td className="bold center large">{Math.floor((props.mental + props.e + props.e_growth) / 6)}</td>
                 </tr>
                 <tr>
                     <td><SqInput value={props.f} digit={3} onchange={(value: string) => {props.setF(Number(value))}}>F</SqInput></td>
                     <td><SqInput value={props.f_growth} digit={3} onchange={(value: string) => {props.setFGrowth(Number(value))}} /></td>
                     <td>精神力<br />{props.mental + props.f + props.f_growth}</td>
-                    <td>{Math.floor((props.mental + props.f + props.f_growth) / 6)}</td>
+                    <td className="bold center large">{Math.floor((props.mental + props.f + props.f_growth) / 6)}</td>
                 </tr>
                 </tbody>
             </table>
--- ts/components/main_spec.tsx
+++ ts/components/main_spec.tsx
@@ -2,22 +2,30 @@ import * as React from 'react'
 import {BaseStatusData} from "../types"
 
 const MainSpec: React.FC<BaseStatusData & {lv: number}> = (props) => {
-
+    const colStyle: React.CSSProperties = {
+        width: "120px"
+    }
     return (
         <div id="main_spec">
             <table>
+                <colgroup>
+                    <col style={colStyle}/>
+                    <col style={colStyle}/>
+                    <col style={colStyle}/>
+                    <col style={colStyle}/>
+                </colgroup>
                 <tbody>
                 <tr>
                     <td>冒険者レベル+<br />生命力ボーナス=</td>
-                    <td>生命抵抗力<br />{props.lv + Math.floor((props.body + props.d + props.d_growth) / 6)}</td>
+                    <td className="center bold large">生命抵抗力<br />{props.lv + Math.floor((props.body + props.d + props.d_growth) / 6)}</td>
                     <td>冒険者レベル*3+<br />+生命力=</td>
-                    <td>HP<br />{props.lv * 3 + (props.body + props.d + props.d_growth)}</td>
+                    <td className="center bold large">HP<br />{props.lv * 3 + (props.body + props.d + props.d_growth)}</td>
                 </tr>
                 <tr>
                     <td>冒険者レベル+<br />精神力ボーナス=</td>
-                    <td>精神抵抗力<br />{props.lv + Math.floor((props.mental + props.f + props.f_growth) / 6)}</td>
+                    <td className="center bold large">精神抵抗力<br />{props.lv + Math.floor((props.mental + props.f + props.f_growth) / 6)}</td>
                     <td>魔法使い系技能<br />レベル合計*3+<br />精神力=</td>
-                    <td>MP<br />?</td>
+                    <td className="center bold large">MP<br />?</td>
                 </tr>
 
                 </tbody>
